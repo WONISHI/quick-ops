643c81faa907d806f3e0b30c789503cd7ea5ce60
weizhifeng <18797942292@139.com>
2025-08-18 11:40:45 +0800

    feat:同步

--- src/extension.ts
+++ src/extension.ts
@@ -9,52 +9,25 @@ import { registerTop } from './register/register-top';
 
 export function activate(context: vscode.ExtensionContext) {
   initProperties(vscode.window.activeTextEditor?.document!);
-
-  // const serverModule = context.asAbsolutePath(path.join('out', 'server.js'));
-  // const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-  // const serverOptions = {
-  //   run: { module: serverModule, transport: TransportKind.ipc },
-  //   debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
-  // };
-
-  // const clientOptions: LanguageClientOptions = {
-  //   documentSelector: [
-  //     { scheme: 'file', language: 'javascript' },
-  //     { scheme: 'file', language: 'typescript' },
-  //     { scheme: 'file', language: 'javascriptreact' },
-  //     { scheme: 'file', language: 'typescriptreact' },
-  //     { scheme: 'file', language: 'vue' },
-  //     { scheme: 'file', language: 'html' },
-  //   ],
-  //   synchronize: {
-  //     fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
-  //   },
-  // };
-
-  // client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
-
-  // client.start().catch((err) => {
-  //   vscode.window.showErrorMessage(`Language Server 启动失败: ${err.message}`);
-  //   console.error('LanguageClient start error:', err);
-  // });
-
   vscode.workspace.onDidChangeTextDocument((e) => {
     properties.content = e.document.getText();
     properties.fileType = e.document.languageId;
   });
 
+  // 注册全局配置
   registerConfig(context)?.then((res: EnvConfProps) => {
+    // 局部搜索
     registerAreaSearch(context, res);
+    // 代码补全
     registerCompletion(context, res);
+    // 定位文件
     registerExtension(context, res);
+    // 回到顶部
     registerTop(context);
   });
 }
 
 export async function deactivate() {
-  // if (client && client.state === 2) { // 2 = Running
-  //   await client.stop();
-  // }
   if (decorationType) {
     decorationType.dispose();
   }
