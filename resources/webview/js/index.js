eac61903b8babc2a29d96526e34ec798ae53b913
weizhifeng <18797942292@139.com>
2025-09-30 10:23:58 +0800

    feat:调整webview

--- resources/webview/js/index.js
+++ resources/webview/js/index.js
@@ -1,27 +1,27 @@
 (function () {
-
   // 全局注册 Element UI
   Vue.use(ELEMENT);
 
   // 定义组件（可以写 template）
   Vue.component('webview-menu', {
-    props: ['catalogue'],
+    props: ['catalogue', 'value'],
     data() {
       return { activeName: 'shell' };
     },
     template: `
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane
-              v-for="(item,index) in catalogue"
-              :key="index"
-              :label="item.label"
-              :name="item.value">
-            </el-tab-pane>
-          </el-tabs>
-        `,
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane
+          v-for="(item,index) in catalogue"
+          :key="index"
+          :label="item.label"
+          :name="item.value">
+        </el-tab-pane>
+      </el-tabs>
+    `,
     methods: {
       handleClick(tab) {
-        console.log('点击了：', tab.name);
+        // 使用 v-model 双向绑定
+        this.$emit('input', this.catalogue[tab.index].value);
       },
     },
   });
@@ -36,54 +36,64 @@
         { label: '设置', value: 'settings' },
       ],
       loading: true,
-      status: 1,
-      vscode:null,
+      activeName: 'shell',
+      vscode: null,
       tableData: [],
     },
     template: `
-          <div class="webview-menu">
-            <webview-menu :catalogue="useCatalogue"></webview-menu>
-            <div>
-              <el-table :data="tableData" style="width: 100%">
-                <el-table-column
-                  prop="index"
-                  align="center"
-                  label="序号"
-                  min-width="10%">
-                </el-table-column>
-                <el-table-column
-                  prop="name"
-                  label="指令名称"
-                  align="center"
-                  min-width="30%">
-                </el-table-column>
-                <el-table-column
-                  min-width="40%"
-                  align="center"
-                  label="指令">
-                  <template slot-scope="scope">
-                    <el-button @click="handleClick(scope.row)" type="text" size="small">{{scope.row.cmd}}</el-button>
-                  </template>
-                </el-table-column>
-                <el-table-column
-                  label="操作"
-                  min-width="20%">
-                  <template slot-scope="scope">
-                    <el-button @click="handleClick(scope.row)" type="text" size="small">运行</el-button>
-                  </template>
-                </el-table-column>
-              </el-table>
-            </div>
-          </div>
-        `,
+      <div class="webview-menu" v-loading="loading">
+        <!-- 使用v-model进行绑定 -->
+        <webview-menu :value="activeName" @input="activeName = $event" :catalogue="useCatalogue"></webview-menu>
+        
+        <!-- shell 选项卡的内容 -->
+        <div v-show="activeName === 'shell'">
+          <el-table :data="tableData" style="width: 100%">
+            <el-table-column
+              prop="index"
+              align="center"
+              label="序号"
+              min-width="10%">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="指令名称"
+              align="center"
+              min-width="30%">
+            </el-table-column>
+            <el-table-column
+              min-width="40%"
+              align="center"
+              label="指令">
+              <template slot-scope="scope">
+                <el-button @click="handleClick(scope.row)" type="text" size="small">{{scope.row.cmd}}</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="操作"
+              min-width="20%">
+              <template slot-scope="scope">
+                <el-button @click="handleClick(scope.row)" type="text" size="small">运行</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    `,
     mounted() {
+      // 获取 vscode API
       this.vscode = acquireVsCodeApi();
+      if (!this.vscode) {
+        console.error("Failed to acquire VSCode API.");
+        return;
+      }
+
+      // 接收 Webview 传来的消息
       window.addEventListener('message', (event) => {
         const { type, data } = event.data;
-        this.vscode.postMessage({ type: 'debug', data: `${JSON.stringify(event)}` });
         if (['ready', 'update'].includes(type)) {
           const scripts = data?.scripts || {};
           if (Object.keys(scripts).length) {
+            // 填充表格数据
             this.tableData = Object.keys(scripts).reduce((prev, key, index) => {
               prev.push({
                 index: index + 1,
@@ -95,6 +105,7 @@
             this.status = `${JSON.stringify(this.tableData)}`;
             this.loading = false;
             this.$nextTick(() => {
+              // 使用 GSAP 动画效果
               gsap.from('.el-table .el-table__body-wrapper .el-table__row', {
                 opacity: 0,
                 y: 50,
@@ -108,8 +119,10 @@
       });
     },
     methods: {
+      // 运行命令的方法
       handleClick(row) {
         const cmd = `npm run ${row.name}`;
+        // 向 VSCode 发送消息
         this.vscode.postMessage({ type: 'run', command: cmd });
       },
     },
--- src/module/mixin-beforePluginInit.ts
+++ src/module/mixin-beforePluginInit.ts
@@ -1,38 +1,39 @@
 import * as vscode from 'vscode';
-import * as path from "path";
-import * as fs from "fs";
+import * as path from 'path';
+import * as fs from 'fs';
 import { MergeProperties } from '../global-object/properties';
 import { findPackageJsonFolder } from '../module/mixin-config';
 import NotificationService from '../utils/notificationService';
 // 加载插件自带的代码片段
 export default async function beforePluginInit(context: vscode.ExtensionContext) {
   MergeProperties({ rootFilePath: await findPackageJsonFolder() });
+
   // MergeProperties({ snippets: await MixinReadSnippets() });
 
-    // 注册创建文件的命令
-    let disposable = vscode.commands.registerCommand('extension.createLogrcFile', async () => {
-      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-      if (!workspaceFolder) {
-        NotificationService.warn('请先打开一个工作区。');
-        return;
-      }
-  
-      const logrcPath = path.join(workspaceFolder.uri.fsPath, '.logrc');
-      // 读取插件自身的配置文件
-      const pluginConfigPath = path.join(context.extensionPath, 'resources', 'template', 'logrc-template.json');
-      const fileContent = fs.readFileSync(pluginConfigPath, 'utf8'); // 或者一个空 JSON 对象
-  
-      try {
-        // 1. 写入文件内容
-        const fileUri = vscode.Uri.file(logrcPath);
-        await vscode.workspace.fs.writeFile(fileUri, Buffer.from(fileContent));
-        NotificationService.info('.logrc 文件已创建！');
-        // 2. 打开并显示这个文件
-        const document = await vscode.workspace.openTextDocument(fileUri);
-        await vscode.window.showTextDocument(document, vscode.ViewColumn.One);
-      } catch (error) {
-        NotificationService.error(`创建文件失败: ${error}`);
-      }
-    });
-    context.subscriptions.push(disposable);
+  // 注册创建文件的命令
+  let disposable = vscode.commands.registerCommand('extension.createLogrcFile', async () => {
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+    if (!workspaceFolder) {
+      NotificationService.warn('请先打开一个工作区。');
+      return;
+    }
+
+    const logrcPath = path.join(workspaceFolder.uri.fsPath, '.logrc');
+    // 读取插件自身的配置文件
+    const pluginConfigPath = path.join(context.extensionPath, 'resources', 'template', 'logrc-template.json');
+    const fileContent = fs.readFileSync(pluginConfigPath, 'utf8'); // 或者一个空 JSON 对象
+
+    try {
+      // 1. 写入文件内容
+      const fileUri = vscode.Uri.file(logrcPath);
+      await vscode.workspace.fs.writeFile(fileUri, Buffer.from(fileContent));
+      NotificationService.info('.logrc 文件已创建！');
+      // 2. 打开并显示这个文件
+      const document = await vscode.workspace.openTextDocument(fileUri);
+      await vscode.window.showTextDocument(document, vscode.ViewColumn.One);
+    } catch (error) {
+      NotificationService.error(`创建文件失败: ${error}`);
+    }
+  });
+  context.subscriptions.push(disposable);
 }
