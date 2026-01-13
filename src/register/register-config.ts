64a067655322e258a1ac133af351a0a0d4c5253c
weizhifeng <18797942292@139.com>
2026-01-13 11:10:43 +0800

    feat: 移除不重要的文件

--- src/register/register-config.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import beforePluginInit from '../module/stages/beforePluginInit';
-import onFileLoad from '../module/stages/onFileLoad';
-
-export async function registerConfig(context: vscode.ExtensionContext) {
-  // 初始化注册其他内容
-  await beforePluginInit(context);
-  onFileLoad(context);
-}
--- src/services/ConditionalCompletionItem.ts
+++
@@ -1,6 +0,0 @@
-import { CompletionItem } from 'vscode';
-import { properties } from '../global-object/properties';
-
-export default class ConditionalCompletionItem extends CompletionItem {
-  checkFn: ((dp: typeof properties) => boolean) | null | undefined;
-}
