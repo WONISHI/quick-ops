0cbbce1158bf728f7481c8e574d28f6306320960
weizhifeng <18797942292@139.com>
2026-01-22 22:33:18 +0800

    feat:优化代码片段补全

--- src/core/types/snippet.ts
+++ src/core/types/snippet.ts
@@ -2,6 +2,7 @@ export interface ISnippetItem {
   prefix: string;
   body: string[];
   description?: string;
+  origin?: string;
   params?: Record<string, any>;
   scope?: string[]; // e.g., ["vue", "vue2", "react"]
   style?: string; // markdown 代码块的语言，如 'vue', 'html'
