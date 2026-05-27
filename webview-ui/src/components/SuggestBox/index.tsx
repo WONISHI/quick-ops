987d8b2fccba7b05f08ed91985d9a5ac9d7390d3
weizhifeng <>
2026-05-27 23:02:55 +0800

    feat: 调整

--- webview-ui/src/components/SuggestBox/index.tsx
+++ webview-ui/src/components/SuggestBox/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import { forwardRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { escapeRegExp } from '../../utils';
