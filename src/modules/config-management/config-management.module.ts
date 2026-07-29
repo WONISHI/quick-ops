4f3cc0f8f05e7cd46cc527447f6a5a4f42033ff9
weizhifeng <18797942292@139.com>
2026-07-29 14:50:32 +0800

    feat: 调整导入顺序

--- src/modules/config-management/config-management.module.ts
+++ src/modules/config-management/config-management.module.ts
@@ -1,6 +1,6 @@
-import type { QuickOpsModule } from '@core/module/quick-ops-module.interface';
 import { CommonModule } from '@common/common.module';
 import { ConfigManagementController } from '@modules/config-management/config-management.controller';
+import type { QuickOpsModule } from '@core/module/quick-ops-module.interface';
 
 export const ConfigManagementModule: QuickOpsModule = {
   imports: [CommonModule],
